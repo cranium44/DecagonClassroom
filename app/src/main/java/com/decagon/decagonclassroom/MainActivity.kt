d39e15a5c44959133af5bbdd452e331862b9f574
Adabe Echaim <36205010+cranium44@users.noreply.github.com>
2019-11-23 02:03:38 +0100

    created home and profile fragments, added to navbar

--- app/src/main/java/com/decagon/decagonclassroom/MainActivity.kt
+++ app/src/main/java/com/decagon/decagonclassroom/MainActivity.kt
@@ -1,18 +1,50 @@
 package com.decagon.decagonclassroom
 
-import android.content.Intent
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
+
+    override fun onNavigationItemSelected(p0: MenuItem): Boolean {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val homeFragment = HomeFragment()
+        val profileFragment = ProfileFragment()
 
-        hello.setOnClickListener {
-            startActivity(Intent(applicationContext, LoginActivity::class.java))
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.layout_fragment_container, homeFragment)
+            .commit()
+        bottom_navigation_view.selectedItemId = R.id.home_nav
+
+
+        bottom_navigation_view.setOnNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.home_nav -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.layout_fragment_container, homeFragment)
+                        .commit()
+                    true
+                }
+                R.id.profile_nav -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.layout_fragment_container, profileFragment)
+                        .commit()
+                    true
+                }
+                else -> {
+                    false
+                }
+            }
         }
+
     }
+
+
 }
