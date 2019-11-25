b88c43503d9f5e1891ddfe06229a3224531de710
Adabe Echaim <36205010+cranium44@users.noreply.github.com>
2019-11-25 01:55:59 +0100

    created the resources tabView fragments

---
+++ app/src/main/java/com/decagon/decagonclassroom/views/ArticlesFragment.kt
@@ -0,0 +1,25 @@
+package com.decagon.decagonclassroom.views
+
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.decagon.decagonclassroom.R
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class ArticlesFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_articles, container, false)
+    }
+
+
+}
