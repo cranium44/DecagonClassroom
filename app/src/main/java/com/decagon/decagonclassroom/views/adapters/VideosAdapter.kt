2e585e37d335bf98fde633a3d9fa92119211405e
Adabe Echaim <36205010+cranium44@users.noreply.github.com>
2019-11-25 02:15:02 +0100

    completed the methods on the adapters

--- app/src/main/java/com/decagon/decagonclassroom/views/adapters/VideosAdapter.kt
+++ app/src/main/java/com/decagon/decagonclassroom/views/adapters/VideosAdapter.kt
@@ -1,3 +1,27 @@
 package com.decagon.decagonclassroom.views.adapters
 
-class VideosAdapter
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.decagon.decagonclassroom.R
+import com.decagon.decagonclassroom.models.Video
+
+class VideosAdapter(var data: ArrayList<Video>) :
+    RecyclerView.Adapter<VideosAdapter.VideoViewHolder>() {
+    class VideoViewHolder(view: View) : RecyclerView.ViewHolder(view)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): VideoViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.videos_row, parent, false)
+        return VideoViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return data.size
+    }
+
+    override fun onBindViewHolder(holder: VideoViewHolder, position: Int) {
+
+    }
+
+}
